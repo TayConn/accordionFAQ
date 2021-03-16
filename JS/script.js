88b3f205055ff25c98420e2428f507b05ba7ec8c
Taylor <tayjoconn@gmail.com>
2021-03-15 21:45:33 -0700

    js bold questions, styling

--- JS/script.js
+++ JS/script.js
@@ -2,14 +2,24 @@ const accordionContent = document.querySelectorAll(`button.accordion_question`);
 
 
 function accordionTog(event) {
+    
     const x = event.target.nextElementSibling;
 
     if(x.style.display==="none") {
         x.style.display = "block";
-       
     } else {
         x.style.display = "none";
     }
+
+
+
+    const y = event.target;
+    if(y.style.fontWeight ==="300"){
+        y.style.fontWeight = "700";
+    } else {
+        y.style.fontWeight = "300";
+    }
+    
 }
 
 accordionContent.forEach(function(accordionToggle){
