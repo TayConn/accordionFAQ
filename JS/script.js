15edb8c4214dfe2cd7b57eb910d069befedd065c
Taylor <tayjoconn@gmail.com>
2021-03-16 10:57:23 -0700

    cleaned

--- JS/script.js
+++ JS/script.js
@@ -16,36 +16,3 @@
     })
 }) ()
 
-
-
-// const accordionContent = document.querySelectorAll(`button.accordion_question`);
-
-
-// function accordionTog(event) {
-    
-//     const x = event.target.closestElement('accordion_content');
-
-//     if(x.style.display==="none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-
-//     const y = event.target;
-//     if(y.style.fontWeight ==="300"){
-//         y.style.fontWeight = "700";
-//         y.classList.toggle("open");
-//     } else {
-//         y.style.fontWeight = "300";
-//     }
-    
-//     console.log(y);
-    
-
-// }
-
-// accordionContent.forEach(function(accordionToggle){
-//     accordionToggle.addEventListener(`click`, accordionTog);
-// })
-
-
